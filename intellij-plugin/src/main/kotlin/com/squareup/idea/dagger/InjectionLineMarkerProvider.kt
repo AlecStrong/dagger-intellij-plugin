86ec808f9757a99c46e774f8b9d653d7aaf2484d
Egor Andreevici <egor@squareup.com>
2018-09-14 15:07:12 -0400

    Provides method finder

--- intellij-plugin/src/main/kotlin/com/squareup/idea/dagger/InjectionLineMarkerProvider.kt
+++ intellij-plugin/src/main/kotlin/com/squareup/idea/dagger/InjectionLineMarkerProvider.kt
@@ -5,15 +5,10 @@ import com.intellij.codeInsight.daemon.DefaultGutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT
-import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.idea.refactoring.toPsiFile
 import org.jetbrains.uast.UField
-import org.jetbrains.uast.UFile
 import org.jetbrains.uast.toUElement
-import org.jetbrains.uast.toUElementOfType
 
 class InjectionLineMarkerProvider : LineMarkerProvider {
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
@@ -21,19 +16,10 @@ class InjectionLineMarkerProvider : LineMarkerProvider {
     if (uElement is UField) {
       val injectAnnotation = uElement.findAnnotation("javax.inject.Inject")
       if (injectAnnotation != null) {
-        val targets = mutableListOf<PsiFile>()
-        ProjectFileIndex.getInstance(element.project).iterateContent { vFile ->
-          val psiFile = vFile.toPsiFile(element.project)
-          val uFile = psiFile?.toUElementOfType<UFile>()
-              ?: return@iterateContent true
-          uFile.classes.forEach {
-            println(it)
-            targets += psiFile
-          }
-          return@iterateContent true
-        }
+        val providesMethods = InjectionProvidersFinder().findProvidesMethods(element.project,
+            uElement.type)
         return LineMarkerInfo(element, element.textRange, ICON, UPDATE_ALL, null,
-            DefaultGutterIconNavigationHandler(targets, "title"), LEFT)
+            DefaultGutterIconNavigationHandler(providesMethods, "title"), LEFT)
       }
     }
     return null
---
+++ intellij-plugin/src/main/kotlin/com/squareup/idea/dagger/InjectionProvidersFinder.kt
@@ -0,0 +1,29 @@
+package com.squareup.idea.dagger
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.psi.NavigatablePsiElement
+import com.intellij.psi.PsiType
+import org.jetbrains.kotlin.idea.refactoring.toPsiFile
+import org.jetbrains.uast.UFile
+import org.jetbrains.uast.toUElementOfType
+
+class InjectionProvidersFinder {
+  fun findProvidesMethods(project: Project, type: PsiType): List<NavigatablePsiElement> {
+    val providesMethods = mutableListOf<NavigatablePsiElement>()
+    ProjectFileIndex.getInstance(project).iterateContent { vFile ->
+      val psiFile = vFile.toPsiFile(project)
+      val uFile = psiFile?.toUElementOfType<UFile>()
+          ?: return@iterateContent true
+      uFile.classes
+          .flatMap { it.methods.toList() }
+          .forEach { method ->
+            if (method.returnType == type && method.findAnnotation("dagger.Provides") != null) {
+              providesMethods += method
+            }
+          }
+      return@iterateContent true
+    }
+    return providesMethods
+  }
+}
