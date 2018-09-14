a6b365c8090e35941459eef20fe6a9aef13ee3d9
Alec Strong <astrong@squareup.com>
2018-09-14 13:27:52 -0400

    Add file iterating functionality

--- intellij-plugin/src/main/kotlin/com/squareup/idea/dagger/InjectionLineMarkerProvider.kt
+++ intellij-plugin/src/main/kotlin/com/squareup/idea/dagger/InjectionLineMarkerProvider.kt
@@ -5,21 +5,30 @@ import com.intellij.codeInsight.daemon.DefaultGutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.refactoring.toPsiFile
 import org.jetbrains.uast.UField
+import org.jetbrains.uast.UFile
 import org.jetbrains.uast.toUElement
+import org.jetbrains.uast.toUElementOfType
 
 class InjectionLineMarkerProvider : LineMarkerProvider {
-  init {
-    println("AHHHHHHHHHHHHH")
-  }
-
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
     val uElement = element.toUElement()
     if (uElement is UField) {
       val injectAnnotation = uElement.findAnnotation("javax.inject.Inject")
       if (injectAnnotation != null) {
+        if (uElement.findAnnotation("Inject") != null) {
+          ProjectFileIndex.getInstance(element.project).iterateContent { vFile ->
+            val uFile = vFile.toPsiFile(element.project)?.toUElementOfType<UFile>() ?: return@iterateContent true
+            uFile.classes.forEach {
+              println(it)
+            }
+            return@iterateContent true
+          }
+        }
         return LineMarkerInfo(element, element.textRange, ICON, UPDATE_ALL, null,
             DefaultGutterIconNavigationHandler(listOf(uElement), "title"), LEFT)
       }
